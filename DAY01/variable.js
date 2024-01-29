4e8b18dacb90b65d8eaf8087fb167ed11c3059f3
Tea <macpro@Tea-s-MacBook-Pro.local>
2024-01-29 10:18:58 +0100

    new commit

---
+++ DAY01/variable.js
@@ -0,0 +1,39 @@
+// comments can make code readable
+
+// Welcome to 30DaysOfJavaScript
+
+/* comments can make code readable, easy to reuse and informative
+*/
+
+let firstName = 'Asabaneh';
+
+let isStudent = true;
+
+let whoPlease 
+
+let emptyValue = null
+
+
+let variable1;
+
+let variable2;
+
+let variable3;
+
+let firstVariable = 'hello';
+let secondVariable = 42;
+let thirdVariable = true;
+let fourthVariable = null;
+
+
+let firstName = 'Oloyin';
+let secondName = 'Tea';
+let maritalStatus = 'Single';
+let country = 'Nigeria';
+let age = 25;
+
+
+let firstName = 'Oloyin', secondName = 'Tea', maritalStatus = 'Single', country = 'Nigeria'; age = 25;
+
+let myAge = 25;
+let yourAge = 30;
