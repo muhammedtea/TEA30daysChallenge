d3176095eae8055c270e8589099e54f1b0710d26
Tea <macpro@Tea-s-MacBook-Pro.local>
2024-01-31 10:16:06 +0100

    push commit

---
+++ DAY03/index.js
@@ -0,0 +1,43 @@
+let firstName = 'Tea';
+let lastName = 'Oloyin';
+let country = 'Nigeria';
+let city = 'Ilorin';
+let age = 25;
+let isMarried = 'Single';
+
+console.log(typeof (firstName));
+console.log(typeof (lastName));
+console.log(typeof (country));
+console.log(typeof (city));
+console.log(typeof (age));
+console.log(typeof (isMarried));
+
+console.log('10' === 10);
+console.log('9.8' === 10);
+
+let number1 = 10;
+let number2 = 5;
+let result2 = number1 > number2;
+
+console.log(result2);
+
+let myString = "This is a truthy string";
+let result3 = myString.length > 0;
+
+console.log(result3);
+
+const now = new Date ();
+console.log(now.getFullYear());
+
+console.log(now.getMonth());
+
+console.log(now.getDay());
+
+console.log(now.getHours());
+
+console.log(now.getMinutes());
+
+prompt('Enter Base:');
+prompt('Enter height');
+prompt('Calc The area of the triangle ');
+
