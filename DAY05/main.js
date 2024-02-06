2157df22d214ed49faba6a22b0be36e3d8e3f8e9
muhammedtea <thelinguistea@icloud.com>
2024-02-06 11:15:17 +0100

    new commit

---
+++ DAY05/main.js
@@ -0,0 +1,35 @@
+const emptyArray = [];
+
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8];
+
+console.log(myArray.length);
+
+console.log(myArray[0]);
+
+console.log(myArray[4]);
+console.log(myArray.length -1);
+
+const mixedDataTypes = [1, 2, 3, 'red', 'yellow', 'blue'];
+console.log(mixedDataTypes.length);
+
+let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+console.log("Comapanies:", itCompanies);
+console.log("Number of companies:", itCompanies.length);
+console.log("First Company:", itCompanies[0]);
+const middleIndex = Math.floor(itCompanies.length / 2);
+
+console.log("Middle Company:", itCompanies[middleIndex]);
+
+console.log("Second Company:", itCompanies[1]);
+console.log("Third Company:", itCompanies[3]);
+console.log("Fourth Company:", itCompanies[4]);
+
+console.log(itCompanies[0].toUpperCase());
+console.log(itCompanies[1].toUpperCase());
+console.log(itCompanies[2].toUpperCase());
+console.log(itCompanies[4].toUpperCase());
+
+
+
+
+
